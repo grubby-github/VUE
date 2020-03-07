57306a419d9c6d438b1fbe23c1ffa18a93e7580f
grubby-github <re_grubby@hotmail.com>
2020-03-07 18:33:49 +0800

    同步最终版本
    同步最终版本

--- my-project/src/components/test.vue
+++ my-project/src/components/test.vue
@@ -1,6 +1,17 @@
 <template>
   <div>
     <h1>{{ msg }}</h1>
+    <span v-bind:title="message">
+      鼠标悬停几秒钟查看此处动态绑定的提示信息！
+    </span>
+    <p v-if="seen">现在你看到我了</p>
+    <ol>
+      <li v-for="todo in todos" :key='todo.key'>
+        {{ todo.text }}
+      </li>
+    </ol>
+    <button v-on:click="reverseMessage">反转消息</button>
+    <input v-model="message" />
   </div>
 </template>
 
@@ -9,7 +20,20 @@ export default {
   name: 'test',
   data () {
     return {
-      msg: 'test12345'
+      msg: 'test12345',
+      message: '页面加载于 ' + new Date().toLocaleString(),
+      seen: true,
+      todos: [
+        { key: 1, text: '学习 JavaScript' },
+        { key: 2, text: '学习 Vue' },
+        { key: 3, text: '整个牛项目' }
+      ]
+    }
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = '发生反转消息'
+      console.log('发生反转消息')
     }
   }
 }
